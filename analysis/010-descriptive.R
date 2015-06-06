69970609972f9779c7c0472c15bab1ed11b4e2f9
Peter Schmiedeskamp <peter@thoughtspot.net>
2015-06-05 17:44:20 -0700

    density plot

--- analysis/010-descriptive.R
+++ analysis/010-descriptive.R
@@ -23,6 +23,16 @@ write.csv(noise.tab, "./results/figures/noise-tab.csv")
 ## Black carbon
 blackcarbon <- read.csv("./data/blackcarbon-cleaned.csv",
                         header=TRUE, stringsAsFactors=FALSE)
+blackcarbon$X <- NULL
+
+odat <- read.csv("~/Projects/ae51summary/data/spot-sample.csv", header=TRUE,
+                 stringsAsFactors=FALSE, skip=15)
+odat <- odat[odat$Point != "Walk",]
+odat$Point <- mapvalues(odat$Point,
+                        from=c(1:4),
+                        to=c("University Way", "40th St", "15th Ave", "Green wall"))
+odat$outdoor <- TRUE
+names(odat) <- names(blackcarbon)
 
 blackcarbon.plot <- ggplot(blackcarbon, aes(y=BC, x=outdoor)) +
   geom_boxplot() + facet_wrap(~ site) +
@@ -31,9 +41,28 @@ blackcarbon.plot <- ggplot(blackcarbon, aes(y=BC, x=outdoor)) +
 ggsave("./results/figures/blackcarbon-box.pdf", width=6, height=6)
 
 
+oldblackcarbon.plot <- ggplot(odat, aes(y=BC, x=site)) +
+  geom_boxplot() +
+  theme_bw() + xlab("Outdoors") + ylab("Black Carbon")
+
+ggsave("./results/figures/oldblackcarbon-box.pdf", width=6, height=6)
+
+
 blackcarbon.tab <- ddply(blackcarbon, .(site, outdoor), summarize,
                          N=length(BC),
                          Min=min(BC), Median=median(BC),
                          Mean=mean(BC), Max=max(BC),
                          SD=sd(BC))
 write.csv(blackcarbon.tab, "./results/figures/blackcarbon-tab.csv")
+
+
+
+## Density plots might also be useful for cross-site comparisons
+noise.dens <- ggplot(noise, aes(x=Average, color=site, linetype=outdoor)) +
+  geom_density() + theme_bw() + xlab("Decibels (averaged by 0.1 second intervals)")
+ggsave("./results/figures/noise-density.pdf", width=8, height=4)
+
+
+blackcarbon.dens <- ggplot(blackcarbon, aes(x=BC, color=site, linetype=outdoor)) +
+  geom_density() + theme_bw() + xlab("Black carbon")
+ggsave("./results/figures/blackcarbon-density.pdf", width=8, height=4)
